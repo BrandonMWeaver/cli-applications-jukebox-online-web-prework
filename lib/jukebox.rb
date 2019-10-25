e3e56830cc4b7669cb2c0c543ace2f23afc7cb41
Brandon Weaver <a7xncob@gmail.com>
2019-10-25 22:41:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,7 +13,7 @@ end
 def list(songs)
   index = 0
   while index < songs.size do
-    puts "#{index + 1}. #{songs[index}"
+    puts "#{index + 1}. #{songs[index]}"
     index += 1
   end
 end
@@ -25,10 +25,10 @@ def play(songs)
   index = 0
   while index < songs.size do
     if input == songs[index]
-      puts "Playing #{songs[index}"
+      puts "Playing #{songs[index]}"
       break
     elsif input.to_i == index + 1
-      puts "Playing #{songs[index}"
+      puts "Playing #{songs[index]}"
       break
     else
       puts "Invalid input, please try again"
